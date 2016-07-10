90d5b373b1144b1cbabe20434ecfb8f10dcfcf4f
François Massot <francois.massot@gmail.com>
2016-07-10 20:51:35 +0200

    Make setup.py compatible with python 3

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
-from codecs import open
 from os import path
+from io import open
 
 here = path.abspath(path.dirname(__file__))
 
@@ -11,8 +11,8 @@ with open(path.join(here, 'anpy', '__version.py')) as __version:
     exec(__version.read())
 assert __version__ is not None
 
-with open(path.join(here, 'README.md')) as readme:
-    LONG_DESC = readme.read().decode('utf-8')
+with open(path.join(here, 'README.md'), encoding='utf-8') as readme:
+    LONG_DESC = readme.read()
 
 setup(
     name='anpy',
