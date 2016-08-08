7c2c4bbbd51f1490c6c9dfe31218c438673514b6
François Massot <francois.massot@gmail.com>
2016-08-09 00:31:12 +0200

    Improve setup.py and add makefile

--- anpy/__init__.py
+++ anpy/__init__.py
@@ -1,3 +1,29 @@
-from .__version import __version__
+# -*- coding: utf-8 -*-
 
-__all__ = ['__version__']
+#           _   _ _______     __
+#     /\   | \ | |  __ \ \   / /
+#    /  \  |  \| | |__) \ \_/ /
+#   / /\ \ | . ` |  ___/ \   /
+#  / ____ \| |\  | |      | |
+# /_/    \_\_| \_|_|      |_|
+
+__title__ = 'anpy'
+__version__ = '0.1.8'
+__author__ = 'Regards Citoyens'
+__license__ = 'MIT'
+
+
+from .model import Amendement, AmendementSummary, AmendementSearchResult, QuestionSearchResult, QuestionSummary
+from .service import AmendementSearchService, QuestionSearchService
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
 # -*- coding: utf-8 -*-
 
+import re
 from setuptools import setup
 from os import path
 from io import open
 
 here = path.abspath(path.dirname(__file__))
 
-__version__ = None
-with open(path.join(here, 'anpy', '__version.py')) as __version:
-    exec(__version.read())
-assert __version__ is not None
+with open('anpy/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
 
 with open(path.join(here, 'README.md'), encoding='utf-8') as readme:
     LONG_DESC = readme.read()
 
 setup(
     name='anpy',
-    version=__version__,
-
+    version=version,
     description='Python client for assemblee-nationale.fr website',
     long_description=LONG_DESC,
     license="MIT",
