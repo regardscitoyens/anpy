c8dfddcc74f578fb57ef2ffa0facfc3ee559c095
François Massot <francois.massot@gmail.com>
2016-08-10 00:23:04 +0200

    fix pep8

--- anpy/__init__.py
+++ anpy/__init__.py
@@ -7,17 +7,13 @@
 #  / ____ \| |\  | |      | |
 # /_/    \_\_| \_|_|      |_|
 
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+
 __title__ = 'anpy'
 __version__ = '0.1.8'
 __author__ = 'Regards Citoyens'
 __license__ = 'MIT'
-
-
-from .model import Amendement, AmendementSummary, AmendementSearchResult, QuestionSearchResult, QuestionSummary
-from .service import AmendementSearchService, QuestionSearchService
-
-# Set default logging handler to avoid "No handler found" warnings.
-import logging
 
 try:  # Python 2.7+
     from logging import NullHandler
